6245a3119a7fd436400d5b29fd52cbcc90fa07a2
martanagib <cv.marta1@gmail.com>
2024-01-24 12:11:52 +0100

    commit

--- Meteo/src/assets/components/Results.jsx
+++ Meteo/src/assets/components/Results.jsx
@@ -52,7 +52,7 @@ const Results = () => {
             <p>{printDate}</p>
           </div>
           <div>
-            <h2>{city.name}</h2>
+            <h2>{city.name} {city.main.temp}°c</h2>
           </div>
           <div className="descrizione">
             <h4>{city.weather[0]?.main}</h4>
